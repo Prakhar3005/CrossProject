861087da90c4daccbf8c607b016d2e5d2a1b654a
Prakhar3005 <prakharkhandelwal181@gmail.com>
2024-11-16 14:56:50 +0530

    new-name

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
             >
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span className="ml-3 text-xl text-white">ElectEcho</span>
+            <span className="ml-3 text-xl text-white">ElectoSphere</span>
           </h2>
           <p className="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
             © 2024
